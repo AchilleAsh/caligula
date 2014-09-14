70e35d2c376e8c1bead0631928235a58d543cf11
Theo Segonds <theo.segonds@gmail.com>
2014-09-14 20:54:28 +0200

    changement de l'url avec la modification de 2015 changement des commentaires en docstring changement de l'usage

--- caligula.py
+++ caligula.py
@@ -4,13 +4,13 @@
 
   # ----------------------------------------------------------------------------
   # "THE BEER-WARE LICENSE" (Revision 42):
-  # <showok@showok.info> wrote this file from the original work of 
+  # <showok@showok.info> wrote this file from the original work of
   # <anthony.perard@gmail.com> : https://github.com/sheep/Chronos.
-  # As long as you retain this notice you  can do whatever you want with this stuff. 
-  # If we meet some day, and you think this stuff is worth it, you can buy me a beer 
+  # As long as you retain this notice you  can do whatever you want with this stuff.
+  # If we meet some day, and you think this stuff is worth it, you can buy me a beer
   # in return Théo Segonds
   # ----------------------------------------------------------------------------
- 
+
 # Modified by MGK from https://github.com/show0k/caligula/blob/master/caligula.py
 #
 # Conformément aux instructions données sur https://github.com/show0k/caligula/blob/master/README.md ,
@@ -30,8 +30,11 @@ import pytz
 
 login = ""
 
-# Parseur du résultat de tree.jsp pour récupérer le nom des items et leur identifiant dans les branches ouvertes
+
 class branchParser(HTMLParser):
+	"""
+	Parseur du résultat de tree.jsp pour récupérer le nom des items et leur identifiant dans les branches ouvertes
+	"""
 	def __init__(self):
 		self.itemsID = []
 		self.itemsNames = []
@@ -55,16 +58,19 @@ class branchParser(HTMLParser):
 			self.category = re.sub("\'\)","",re.sub(r'javascript:openCategory\(\'',"",attrs[0][1]))
 	def handle_endtag(self, tag):
 		if tag == "span" and self.treeitem==True:
-			self.treeitem=False			
+			self.treeitem=False
 		if tag == "a" and self.treeitem==True:
 			self.ina = False
-			
+
 	def handle_data(self, data):
 		if self.ina:
 			self.itemsNames.append(data)
 
-## Parseur du résultat de tree.jsp pour récupérer le nom et l'identifiant des branches
+
 class categoryParser(HTMLParser):
+	"""
+	Parseur du résultat de tree.jsp pour récupérer le nom et l'identifiant des branches
+	"""
 	def __init__(self):
 		self.branches = []
 		self.branchesNames = []
@@ -84,34 +90,38 @@ class categoryParser(HTMLParser):
 			self.treebranch=False
 		if tag == "a" and self.treebranch==True:
 			self.ina = False
-			
+
 	def handle_data(self, data):
 		if self.ina :
 			self.branchesNames.append(data)
 
-## Parseur du résultat de eventInfo.jsp pour récupérer l'information des évènements en vue de déterminer s'il s'agit de TD, TP, Projets, contrôles, ...
 class eventsParser(HTMLParser):
+	"""
+	Parseur du résultat de eventInfo.jsp pour récupérer l'information des évènements en vue de déterminer s'il s'agit de TD, TP, Projets, contrôles, ...
+	"""
 	def __init__(self):
 		self.result = []
 		self.inp = False
 		self.s = requests.Session()
 		HTMLParser.__init__(self)
-		
+
 	def handle_starttag(self, tag, attrs):
 		if tag == "p":
 			self.inp = True
-			
+
 	def handle_endtag(self, tag):
 		if tag == "p":
 			self.inp = False
-			
+
 	def handle_data(self, data):
 		if self.inp:
 			if not data=="&nbsp;" :
 				self.result = data
 
-## Parseur du résultat de info.jsp (une fois qu'on l'a configuré comme voulu: sélection Id et weeks) pour extraire les données du tableau nécessaires à la création du calendrier
 class infoParser(HTMLParser):
+	"""
+	Parseur du résultat de info.jsp (une fois qu'on l'a configuré comme voulu: sélection Id et weeks) pour extraire les données du tableau nécessaires à la création du calendrier
+	"""
 	global login
 	def __init__(self):
 		self.result = []
@@ -121,7 +131,7 @@ class infoParser(HTMLParser):
 		self.skipping=0
 		self.current_row = []
 		self.current_data = []
-		self.URL0 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=4&login=%s&password=%s' %(login,login)
+		self.URL0 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=1&login=%s&password=%s' %(login,login)
 		self.s = requests.Session()
 		self.html = self.s.get(self.URL0)
 		self.parser = eventsParser()
@@ -177,7 +187,7 @@ class infoParser(HTMLParser):
 					elif self.parser.result.lower().find("tp")>=0:
 						self.current_row.append("TP")
 					elif self.parser.result.lower().find("contrôle")>=0:
-						self.current_row.append("Contrôle")				
+						self.current_row.append("Contrôle")
 
 	def handle_endtag(self, tag):
 		if tag == "table":
@@ -187,12 +197,14 @@ class infoParser(HTMLParser):
 		elif tag == "td":
 			self.end_td()
 
-## format de date pour iCal
 def dateICal(date):
+	"""format de date pour iCal"""
 	return date.strftime("%Y%m%dT%H%M%S")
 
-## création du calendrier a partir de l'information récoltée
 def make_calendar(parsed,pourProf=False):
+	"""
+	création du calendrier a partir du parseur
+	"""
 	# Création de l'agenda
 	cal = Calendar()
 
@@ -230,14 +242,14 @@ def make_calendar(parsed,pourProf=False):
 		if len(i) < 7:
 			continue
 		start = datetime.strptime("%s %s" % (i[0], i[1]), "%d/%m/%Y %H:%M")
-		
+
 		if start.hour == 10:
 			start = start + timedelta(minutes = 10)
 		elif start.hour == 13:
 			start = start + timedelta(minutes = 15)
 		elif start.hour == 15:
 			start = start + timedelta(minutes = 25)
-		
+
 		if re.match("^\d{1,2}h$", i[2]):
 			delta = datetime.strptime(i[2], "%Hh")
 
@@ -264,7 +276,7 @@ def make_calendar(parsed,pourProf=False):
 		room = i[7][:5]
 
 		name = unicodedata.normalize('NFKD', i[4]).encode('ascii','ignore')
-		
+
 		typeevent = i[3]
 		if typeevent == "TP" and name.lower().find("projet")>=0:
 			typeevent = ""
@@ -278,7 +290,7 @@ def make_calendar(parsed,pourProf=False):
 				summary = "%s avec %s en %s" %(name,groups,room)
 		else:
 			summary = "%s avec %s en %s" %(name,prof,room)
-		
+
 		uid =  "%s-%s@%s" % (dateICal(start),dateICal(end), room) #event_condensed_name[:10])
 
 		# Pour ajouter le timezone proprement à chaque heure d'événements (optionel)
@@ -287,7 +299,7 @@ def make_calendar(parsed,pourProf=False):
 		date_start = [int(d) for d in str(start).split(" ")[0].split('-')]
 		date_end = [int(d) for d in str(end).split(" ")[0].split('-')]
 
-		# Le fichier de sortie ne doit pas dépasser 75 caractères par ligne 
+		# Le fichier de sortie ne doit pas dépasser 75 caractères par ligne
 		event = Event()
 		event.add('summary',summary)
 		event.add('location',room+", ENSEA, Cergy")
@@ -301,8 +313,10 @@ def make_calendar(parsed,pourProf=False):
 		cal.add_component(event)
 	return cal
 
-## Parser to get the number of the current week
 class weekParser(HTMLParser):
+	"""
+	Parser to get the number of the current week
+	"""
 	def __init__(self):
 		self.pianoSelected = False
 		self.nweek = 1
@@ -320,7 +334,7 @@ class weekParser(HTMLParser):
 
 # Transform un fichier iCal en JSON
 def ical_to_json(ics):
- 
+
 	cal = ics
 	data = {}
 	data[cal.name] = dict(cal.items())
@@ -338,7 +352,7 @@ def ical_to_json(ics):
 
 def get_html_agenda(param_lst,debug = True):
 	global login
-	URL1 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=4&login=%s&password=%s' %(login,login)
+	URL1 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=1&login=%s&password=%s' %(login,login)
 	tree = "http://caligula.ensea.fr/ade/standard/gui/tree.jsp"
 	s = requests.Session()
 	s.get(URL1)
@@ -359,7 +373,7 @@ def get_html_agenda(param_lst,debug = True):
 
 			url = "%s?%s&expand=false&forceLoad=false&reload=false" % (tree, branch)
 			r = s.get(url)
-			
+
 	if param_lst[3] != 0 :
 		selectId = "selectId=%i" % param_lst[3]
 		url = "%s?%s&expand=false&forceLoad=false&reload=false" % (tree, selectId)
@@ -368,7 +382,7 @@ def get_html_agenda(param_lst,debug = True):
 	# Get the time bar
 	url = "http://caligula.ensea.fr/ade/custom/modules/plannings/pianoWeeks.jsp"
 	result = s.get(url)
-	
+
 	parser = weekParser()
 	parser.feed(result.content)
 	parser.close()
@@ -395,8 +409,13 @@ def get_html_agenda(param_lst,debug = True):
 
 	return content
 
-## Fournit les identifiants en fonction des groupes de TPTD : à faire par le parser
+
 def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
+	"""
+	Fournit les identifiants en fonction des groupes de TPTD
+	Attention, ce code est très moche, il faut de préférence utiliser le
+	parser de la recherche
+	"""
 	annee = td = tp = 0
 	option = ""
 	if user[1].lower() == 'g'  and user_type in 'stagiaires': #1A 2A
@@ -405,21 +424,21 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 		if(len(user) > 5):
 			td = int(user[5])
 			if(len(user) > 8):
-				tp = int(user[8])	
+				tp = int(user[8])
 
 	elif user[0] == '3':
 		if user[1:3].lower() in 'aei':
 			option = 'aei'
 			tp = user[1:3]
 
-	elif user[0:2].lower() in 'sic': 
+	elif user[0:2].lower() in 'sic':
 		td = 'sic'
 		option = 'mastere'
 		if len(user)> 3:
 			tp = int(user[5])
 
 
-	elif user[0:2].lower() in 'esa': 
+	elif user[0:2].lower() in 'esa':
 		td = 'esa'
 		option = 'mastere'
 		if len(user)> 3:
@@ -451,7 +470,7 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 4
 					elif tp == 4:
-						param[3] = 5		
+						param[3] = 5
 				elif td == 3:
 					param[2] = 74
 					if tp == 5:
@@ -471,13 +490,13 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 10
 					elif tp == 4:
-						param[3] = 12		
+						param[3] = 12
 				elif td == 3:
 					param[2] = 383
 					if tp == 5:
 						param[3] = 384
 					elif tp == 6:
-						param[3] = 142			
+						param[3] = 142
 			elif groupe == 3:
 				param[1] = 63
 				if td == 1:
@@ -491,13 +510,13 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 16
 					elif tp == 4:
-						param[3] = 17		
+						param[3] = 17
 				elif td == 3:
 					param[2] = 79
 					if tp == 5:
 						param[3] = 18
 					elif tp == 6:
-						param[3] = 441	
+						param[3] = 441
 
 		elif annee == 2:
 			param[1] = 64
@@ -513,7 +532,7 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 22
 					elif tp == 4:
-						param[3] = 23		
+						param[3] = 23
 				elif td == 3:
 					param[2] = 386
 					if tp == 5:
@@ -532,13 +551,13 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 26
 					elif tp == 4:
-						param[3] = 27		
+						param[3] = 27
 				elif td == 3:
 					param[2] = 496
 					if tp == 5:
 						param[3] = 497
 					elif tp == 6:
-						param[3] = 498		
+						param[3] = 498
 			elif groupe == 3:
 				if td == 1:
 					param[2] = 85
@@ -551,13 +570,13 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 					if tp == 3:
 						param[3] = 31
 					elif tp == 4:
-						param[3] = 32		
+						param[3] = 32
 				elif td == 3:
 					param[2] = 389
 					if tp == 5:
 						param[3] = 391
 					elif tp == 6:
-						param[3] = 398	
+						param[3] = 398
 		elif option in 'masteres':
 			param[1] = 298
 			if td == 'sic':
@@ -580,7 +599,7 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 		# elif annee == '3D' :
 		# 	param[1] = 62
 		# elif annee == '3A' :
-		# 	param[1] = 62	
+		# 	param[1] = 62
 		# else :
 		# 	print 'Cette combinaison est invalide'
 
@@ -590,12 +609,15 @@ def get_user_config(user_type = 'stagiaires', user = '2G1TD1TP1'):
 
 	return user,param
 
-## Récupère l'agenda lié à un élément de la base (ne convient pas pour
-# les groupes de TD qui sont des branches, pas des items)
+
 def fetch_ical(param, user, path_destination = ' ',debug=False):
+	"""
+	Récupère l'agenda lié à un élément de la base (ne convient pas pour
+	les groupes de TD qui sont des branches, pas des items)
+	"""
 	if not os.path.exists(path_destination) and len(path_destination) > 2:
 		os.mkdir(path_destination[:-1])
-	
+
 	html = get_html_agenda(param,debug=debug)
 	parser = infoParser()
 	parser.feed(html)
@@ -604,7 +626,7 @@ def fetch_ical(param, user, path_destination = ' ',debug=False):
 		ical =  make_calendar(parser.result,pourProf=True)
 	else:
 		ical =  make_calendar(parser.result,pourProf=False)
-	
+
 	ical_str = str(ical.to_ical())
 
 	if debug == True:
@@ -625,8 +647,10 @@ def fetch_ical(param, user, path_destination = ' ',debug=False):
 		f.write(ical_str)
 	print user,param,size
 
-## Récupère les agendas de tous les groupes d'élèves
 def fetch_all_ical(path_destination = 'ics/',debug = False):
+	"""
+	Récupère les agendas de tous les groupes d'élèves
+	"""
 
 	# 1A et 2A
 	for annee in range(1,3):
@@ -642,19 +666,28 @@ def fetch_all_ical(path_destination = 'ics/',debug = False):
 		for tp in range (1,4):
 			fetch_ical(user= "%sTP%s" %(master,tp),path_destination=path_destination)
 
-## Effectue la recherche d'un élément de la base (groupe, prof, salle) par l'intermédiaire de l'outil de recherche du site
+
 def search_item(name):
+	"""
+	Effectue la recherche d'un élément de la base (groupe, prof, salle) par l'intermédiaire de l'outil de recherche du site
+	"""
 	global login
 	print "Recherche d'un élément de la base dont le nom contient \""+name+"\""
 	param_lst = ['',0,0,0]
-	URL1 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=4&login=%s&password=%s' % (login,login)
+	URL1 = 'http://caligula.ensea.fr/ade/standard/gui/interface.jsp?projectId=1&login=%s&password=%s' % (login,login)
+
 	s = requests.Session()
 	s.get(URL1)
-
-	tree = 'http://caligula.ensea.fr/ade/standard/gui/tree.jsp?projectId=4&login=%s&password=%s' % (login,login)
+	tree = 'http://caligula.ensea.fr/ade/standard/gui/tree.jsp?projectId=1&login=%s&password=%s' % (login,login)
 	search_form = {'search': name}
 	s = requests.post(tree, data=search_form)
-	
+
+	if "<HTML>" not in s.content :
+		print "%s est probablement un mauvais mot de passe ou il y a eu un changement dans le site. Si tel est le cas, contactez moi sur showok@showok.info" % login
+		sys.exit(2)
+	if "Veuillez vous identifier" in s.content :
+		print "il semblerait qu'il y ai eu une modification dans le site"
+
 	parser = branchParser()
 	parser.itemsID = []
 	parser.itemsNames = []
@@ -662,7 +695,6 @@ def search_item(name):
 	parser.close()
 	if len(parser.itemsID)==0: # si aucun résultat n'est trouvé
 		print "%s ne correspond à aucune donnée de la base" % name
-		print "Le mot de passe entré est peut-être mauvais"
 		sys.exit(2)
 	elif len(parser.itemsID)==1: # dans le cas optimal où un seul résultat est renvoyé
 		user = parser.itemsNames[0]
@@ -680,20 +712,20 @@ def search_item(name):
 	return user,param_lst
 
 def usage():
-	
+
 	print 'Usage : '
-	print 'caligula.py <options> -l <login de caligula.ensea.fr>'
+	print 'caligula.py <options> -l <login pour acceder à caligula.ensea.fr>'
 	print ''
 	print 'Options:'
 	print '-s --search 	partie du nom de prof, groupe de TD ou salle'
-	print 'exemple 1 : caligula.py -s guerquin -l login'
-	print 'exemple 2 : caligula.py -s C104 -l login'
-	print 'exemple 3 : caligula.py -s \'1G1 TP3\' -l login'
+	print 'exemple 1 : caligula.py -s guerquin -l <login pour acceder à caligula.ensea.fr>'
+	print 'exemple 2 : caligula.py -s C104 -l <login pour acceder à caligula.ensea.fr>'
+	print 'exemple 3 : caligula.py -s \'1G1 TP3\' -l <login pour acceder à caligula.ensea.fr>'
 	print ''
 	print '-g --groupe 	groupe complet avec la syntaxe GxTDxTPx'
 	print '			all extraira tous les groupes'
-	print 'exemple : caligula.py -g 2G1TD1TP1 -l login'
-	print 'exemple : caligula.py -g all -l login'
+	print 'exemple : caligula.py -g 2G1TD1TP1 -l <login pour acceder à caligula.ensea.fr>'
+	print 'exemple : caligula.py -g all -l <login pour acceder à caligula.ensea.fr>'
 	print ''
 
 
@@ -717,13 +749,13 @@ def main(argv):
 		elif opt in ("-d", "--debug"): debug = True
 		elif opt in ("-s", "--search"): search = arg
 		elif opt in ("-l", "--login"): login = arg
-		else : 
+		else :
 			usage()
 			sys.exit(2)
-			
-	
-		
-		
+
+
+
+
 
 	# if len(groupe) != 9  and groupe != 'all':
 	# 	print "Le choix '%s' est incorrect" %(groupe)
@@ -740,8 +772,8 @@ def main(argv):
 	if not(search is ""):
 		user,param = search_item(search)
 		fetch_ical(param=param,user=user,debug = debug)
-	
+
 	sys.exit(2)
-		
+
 if __name__ == "__main__":
 	main(sys.argv[1:])
